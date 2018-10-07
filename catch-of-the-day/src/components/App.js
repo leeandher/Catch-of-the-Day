1ec2e868af4eadc231401372604ff656705b430c
Leander Rodrigues <leandergrodrigues@gmail.com>
2018-10-07 15:51:45 -0400

    Configured load sample data button

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,12 +2,9 @@ import React from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
   state = {
     fishes: {},
     order: {}
@@ -20,6 +17,10 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -27,7 +28,10 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
