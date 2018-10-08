4a5c299246fed15702a27b54593649dcbb11f235
Leander Rodrigues <leandergrodrigues@gmail.com>
2018-10-07 20:46:54 -0400

    Set up local storage and firebase state persistence, and more!

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,17 +4,48 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
+import base from "../base";
 
 class App extends React.Component {
+  //Component State
   state = {
     fishes: {},
     order: {}
   };
 
+  //Lifecycle Methods
+
+  componentDidMount() {
+    const { params } = this.props.match;
+    //Sync state with Firebase
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes"
+    });
+    //Retrieve orders from local storage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) this.setState({ order: JSON.parse(localStorageRef) });
+  }
+
+  componentDidUpdate() {
+    const { params } = this.props.match;
+    localStorage.setItem(params.storeId, JSON.stringify(this.state.order));
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
+  //Custom Functions
+
+  //General Structure for Custom Functions
+  //1. Take a copy of state
+  //2. Perform processes
+  //3. Set state to copy
+
   addFish = fish => {
     const fishes = { ...this.state.fishes };
     fishes[`fish${Date.now()}`] = fish;
-    //Since in ES6 { fishes : fishes } === { fishes }
     this.setState({ fishes });
   };
 
@@ -22,6 +53,13 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   };
 
+  addToOrder = key => {
+    const order = { ...this.state.order };
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  };
+
+  //Render Function
   render() {
     return (
       <div className="catch-of-the-day">
@@ -29,11 +67,15 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                details={this.state.fishes[key]}
+                addToOrder={() => this.addToOrder(key)}
+              />
             ))}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
