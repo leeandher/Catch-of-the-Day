1b961ae57b1803d7d46f6fa40a22f37e70e9fe59
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-01-05 14:02:01 -0500

    removed fb and severe dependency

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,9 +11,9 @@ const Login = props => (
     <button className="twitter" onClick={() => props.authenticate("Twitter")}>
       Log in with Twitter
     </button>
-    <button className="facebook" onClick={() => props.authenticate("Facebook")}>
+    {/* <button className="facebook" onClick={() => props.authenticate("Facebook")}>
       Log in with Facebook
-    </button>
+    </button> */}
   </nav>
 );
 
